35b4fc450bc942c03b6b0bdc1ef8c779a935fa93
KaykyOne <kaykyzioti@gmail.com>
2025-08-13 22:09:09 -0300

    tentativa enviar para main 2

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -5,7 +5,7 @@ import { selectAllLeads, updateLead } from '@/app/hooks/hook'
 import { format } from 'date-fns'
 import { useRouter } from 'next/navigation'
 
-export default function page() {
+export default function Page() {
   const router = useRouter()
   const [leads, setLeads] = useState([]);
   const [filtro, setFiltro] = useState('fria');
